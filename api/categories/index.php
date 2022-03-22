d592764ec9d086737341c3bf153eea968ec8c6e4
=Ralph Stone <ralphleestone@gmail.com>
2022-03-21 20:58:29 -0600

    updated categories index.php

---
+++ api/categories/index.php
@@ -0,0 +1,32 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+}
+
+$isId = filter_input(INPUT_GET, "id");
+
+if(isset($isId) && $method == 'GET') {
+    include('./read_single.php');
+}
+else if ($method == 'GET') {
+    include('./categories.php');
+}
+else if ($method == 'POST') {
+    include('./create.php');
+} 
+else if ($method == 'PUT') {
+    include('./update.php');
+}
+else if ($method == 'DELETE') {
+    include('./delete.php');
+}
+else {
+    echo json_encode(
+        array('message' => 'No Quotes Found')
+    );
+}
+?>

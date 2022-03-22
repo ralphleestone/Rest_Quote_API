f0ea45c9077bb0f3295a757813e0f287dd26d2ec
=Ralph Stone <ralphleestone@gmail.com>
2022-03-22 12:55:34 -0600

    update

--- api/categories/create.php
+++ api/categories/create.php
@@ -20,6 +20,12 @@ $data = json_decode(file_get_contents("php://input"));
 $category->id = $data->id;
 $category->category = $data->category;
 
+$quoteExists = IsValid($category->id,$category)
+
+if(!$quoteExists){
+    echo json_encode(array('message' => 'Missing Required Parameters'));
+}
+
 if(isset($category->category) == NULL){
     echo json_encode(array('message' => 'Missing Required Parameters'));
 }

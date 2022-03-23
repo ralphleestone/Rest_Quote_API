cb77a601e93704f84bfb5264fe730aec35ca488c
=Ralph Stone <ralphleestone@gmail.com>
2022-03-23 01:59:16 -0600

    update

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -30,8 +30,8 @@ $quote->quote = $data->quote;
 $quote->authorId = $data->authorId;
 $quote->categoryId = $data->categoryId;
 
-$authorIdExists = IsValid($data->authorId,$quote);
-$categoryIdExists = IsValid($data->categoryId,$quote);
+$authorIdExists = IsValid($quote->authorId,$quote);
+$categoryIdExists = IsValid($quote->categoryId,$quote);
 
 if(!$categoryIdExists){
     echo json_encode(
